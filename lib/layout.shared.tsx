7fc2651753eda3eedf5dfabaf591e22c7cb371f1
kunda <kunda@ADMIN>
2025-10-06 20:42:33 +0530

    Refactor layout components and enhance shared layout options

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,4 +1,6 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { BookIcon } from "lucide-react";
+import Link from "next/link";
 
 /**
  * Shared layout configurations
@@ -13,18 +15,32 @@ export function baseOptions(): BaseLayoutProps {
       title: (
         <>
           <svg
+            xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-blocks-icon lucide-blocks"
           >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
+            <path d="M10 22V7a1 1 0 0 0-1-1H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-5a1 1 0 0 0-1-1H2" />
+            <rect x="14" y="2" width="8" height="8" rx="1" />
           </svg>
-          My App
+          <Link href={"/"}>Random Coders</Link>
         </>
       ),
     },
-    // see https://fumadocs.dev/docs/ui/navigation/links
-    links: [],
+    links: [
+      {
+        icon: <BookIcon />,
+        text: "Blog",
+        url: "/blog",
+        secondary: false,
+      },
+    ],
+    githubUrl: "https://github.com",
   };
 }
